8a0c6c2d94390094efd2140c131f03148ba6ace7
Survivor <990473625@qq.com>
2017-05-11 14:08:30 +0800

    'update'

--- layacmd-native.js
+++ layacmd-native.js
@@ -1,7 +1,22 @@
 const path = require("path");
 const spawn = require("child_process").spawn;
+const
+{
+	printOk,
+	printErr,
+	printQuotation,
+	tr
+} = require("./print.js");
 
 let exe = path.join("node_modules/layanative/out/main.js");
 let args = [exe];
 args.push(process.argv.slice(2));
-spawn("node", args);
+let cp = spawn("node", args);
+cp.stdout.on('data', function(data)
+{
+	printQuotation(data.toString());
+})
+cp.stderr.on('data', function(data)
+{
+	printErr(data.toString());
+})
